d62abb0e90492776b0c515d58ff0b371b81ed2fa
Firas <97522852+fifolio@users.noreply.github.com>
2026-05-01 17:43:01 +0000

    Complete Create + Read funcs

--- backend/config.ts
+++ backend/config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,8 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 const db = getFirestore(app)
 
-export { db, analytics }
+export { db }
